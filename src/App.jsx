4bd938417e3ad6050315f01d4f81568c694b2588
MMM-Akees <akeesak15@gmail.com>
2026-06-08 09:09:30 +0530

    Task 5 Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   {
@@ -43,11 +43,57 @@ function App() {
   {
     /*Task 3 */
   }
-  const [showProfile, setShowProfile] = useState(false);
+  // const [showProfile, setShowProfile] = useState(false);
 
-  const toggleProfile = () => {
-    setShowProfile(!showProfile);
-  };
+  // const toggleProfile = () => {
+  //   setShowProfile(!showProfile);
+  // };
+
+  {
+    /*Task 4 */
+  }
+  // const [expenseName, setExpenseName] = useState("");
+  // const [amount, setAmount] = useState("");
+  // const [expenses, setExpenses] = useState([]);
+
+  // const addExpense = () => {
+  //   if (!expenseName || !amount) {
+  //     return;
+  //   }
+
+  //   const newExpense = {
+  //     name: expenseName,
+  //     amount: Number(amount),
+  //   };
+
+  //   setExpenses([...expenses, newExpense]);
+
+  //   setExpenseName("");
+  //   setAmount("");
+  // };
+
+  // const totalExpense = expenses.reduce(
+  //   (total, expense) => total + expense.amount,
+  //   0,
+  // );
+
+  {
+    /*Task 5 */
+  }
+  const [loading, setLoading] = useState(true);
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setEmployees(["Akees", "Ali", "Niyas", "Fazil"]);
+
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
 
   return (
     <>
@@ -73,7 +119,7 @@ function App() {
 
       {/*Task 3 */}
 
-      <h2>User Profile Toggle</h2>
+      {/* <h2>User Profile Toggle</h2>
 
       <button onClick={toggleProfile}>
         {showProfile ? "Hide Profile" : "Show Profile"}
@@ -85,7 +131,47 @@ function App() {
           <p>Frontend Developer</p>
           <p>Kalmunai</p>
         </div>
-      )}
+      )} */}
+
+      {/*Task 4 */}
+      {/* <h1>Expense Tracker</h1>
+
+      <input
+        type="text"
+        placeholder="Expense Name"
+        value={expenseName}
+        onChange={(e) => setExpenseName(e.target.value)}
+      />
+
+      <input
+        type="number"
+        placeholder="Amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+
+      <button onClick={addExpense}>Add Expense</button>
+
+      <h2>Expense List</h2>
+
+      <ul>
+        {expenses.map((expense, index) => (
+          <li key={index}>
+            {expense.name} - Rs.{expense.amount}
+          </li>
+        ))}
+      </ul>
+
+      <h2>Total Expense: Rs.{totalExpense}</h2> */}
+
+      {/*Task 5 */}
+      <h1>Employee List</h1>
+
+      <ul>
+        {employees.map((employee, index) => (
+          <li key={index}>{employee}</li>
+        ))}
+      </ul>
     </>
   );
 }
